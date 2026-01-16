180e7da4c3f28fd07de7788367b322c88767e3a0
Dababah <pawasalee96@gamil.com>
2026-01-16 21:03:32 +0700

    Implementasikan `FormatUtils` untuk pemformatan mata uang dan persentase
    - Tambahkan fungsi `formatRupiah` untuk mengubah angka menjadi format mata uang Rupiah (IDR).
    - Tambahkan fungsi `formatPersen` untuk memformat angka desimal menjadi string persentase dengan dua angka di belakang koma.

--- app/src/main/java/com/example/smartreturn/utils/FormatUtils.kt
+++ app/src/main/java/com/example/smartreturn/utils/FormatUtils.kt
@@ -1,2 +1,27 @@
 package com.example.smartreturn.utils
 
+
+
+import java.text.NumberFormat
+import java.util.Locale
+
+object FormatUtils {
+
+    /**
+     * REQ-12 & REQ-15: Mengubah angka Double menjadi format Rupiah
+     * Contoh: 1000000 -> Rp 1.000.000
+     */
+    fun formatRupiah(number: Double): String {
+        val localeID = Locale("in", "ID")
+        val numberFormat = NumberFormat.getCurrencyInstance(localeID)
+        return numberFormat.format(number)
+    }
+
+    /**
+     * Memformat angka persentase agar tampil dengan dua angka di belakang koma
+     * Contoh: 0.125 -> 12.5%
+     */
+    fun formatPersen(number: Double): String {
+        return String.format("%.2f%%", number)
+    }
+}
