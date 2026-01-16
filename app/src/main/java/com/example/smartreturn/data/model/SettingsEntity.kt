b490910e87733ff13a7e4064dbb2ef39763bb2e5
Dababah <pawasalee96@gamil.com>
2026-01-16 21:21:11 +0700

    Implementasikan model data `SettingsEntity` untuk Room Database
    - Tambahkan anotasi `@Entity` dengan nama tabel `settings`.
    - Definisikan properti `user_id` sebagai primary key untuk menghubungkan data dengan pengguna.
    - Tambahkan properti `default_kenaikan_emas` dengan nilai default 8.0 dan `default_suku_bunga` dengan nilai default 6.5.

--- app/src/main/java/com/example/smartreturn/data/model/SettingsEntity.kt
+++ app/src/main/java/com/example/smartreturn/data/model/SettingsEntity.kt
@@ -1,2 +1,14 @@
 package com.example.smartreturn.data.model
 
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "settings")
+data class SettingsEntity(
+    @PrimaryKey
+    val user_id: String, // Menghubungkan ke user (sesuai SRS)
+    val default_kenaikan_emas: Double = 8.0,
+    val default_suku_bunga: Double = 6.5
+)
